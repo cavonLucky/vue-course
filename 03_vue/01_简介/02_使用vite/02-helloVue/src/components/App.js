f6382803e7136dde55a426c6079d0d0826880573
cavonLucky <425247833@qq.com>
2023-05-06 15:44:50 +0800

    使用vite-1/2

---
+++ 03_vue/01_简介/02_使用vite/02-helloVue/src/components/App.js
@@ -0,0 +1,39 @@
+/*
+ * @Author: cavonLucky 425247833@qq.com
+ * @Date: 2023-05-06 14:02:02
+ * @LastEditors: cavonLucky 425247833@qq.com
+ * @LastEditTime: 2023-05-06 15:42:17
+ * @FilePath: /vue-course/03_vue/01_简介/02_使用vite/02-helloVue/src/App.js
+ */
+
+// 1. 引入子组件
+import MyButton from "../MyButton";
+
+
+
+// 创建根组件
+export default {
+
+  data() {
+    return {
+      message: "在vue的花园里面挖呀挖呀挖"
+    }
+  },
+
+  // 在组件中注册子组件
+  components: {
+    // MB: MyButton
+    // MyButton: MyButton
+    // MyButton
+    "my-button": MyButton // 如果小写命名最好加引号
+  },
+
+  // template: `
+  //   <h1>{{ message }}</h1>
+  //   <MyButton></MyButton>
+  //   <MyButton></MyButton>
+  //   <MyButton></MyButton>
+  //   <MyButton></MyButton>
+  //   <MyButton></MyButton>
+  // `,
+}
---
+++ 03_vue/01_简介/02_使用vite/02-helloVue/src/index.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: cavonLucky 425247833@qq.com
+ * @Date: 2023-05-06 12:58:45
+ * @LastEditors: cavonLucky 425247833@qq.com
+ * @LastEditTime: 2023-05-06 14:03:44
+ * @FilePath: /vue-course/03_vue/01_简介/02_使用vite/02-helloVue/src/index.js
+ */
+import { createApp } from 'vue/dist/vue.esm-bundler.js';
+import App from './components/App';
+
+// 创建应用并挂载
+// createApp(App).mount('#root');
+
+// mount() 的返回值是根组件App的实例
+const vm = createApp(App).mount('#root');
