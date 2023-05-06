ea6336275d817469ffd70d0d3bf9f5711c0a2643
cavonLucky <425247833@qq.com>
2023-05-06 21:23:25 +0800

    代码分析

---
+++ 03_vue/02_vue的基础/src/App.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Author: cavonLucky 425247833@qq.com
+ * @Date: 2023-05-06 20:02:17
+ * @LastEditors: cavonLucky 425247833@qq.com
+ * @LastEditTime: 2023-05-06 21:15:57
+ * @FilePath: /vue-course/03_vue/02_vue的基础/src/App.vue
+-->
+
+<script>
+// 组件，一个组件可以创建多个组件实例
+// 组件就是一个普通的js对象
+export default {
+  // data 是一个函数，在data中，this就是当前的组件实例vm
+  data() {
+    // this undefined：如果使用箭头函数，则无法通过this来访问组件实例
+    // data: () => {
+    // data: (vm) => {
+    // console.log("vm: ", vm); // Proxy(Object)...
+    // 使用vue时，减少使用箭头函数
+    console.log('this: ', this);
+
+    // 直接向组件实例中添加的属性不会被vue所代理，不是响应数据，修改后页面不会发生变化
+    this.name = '孙悟空'
+
+    // data会返回一个对象作为返回值，vue会对该对象进行代理，从而将其转换为响应式数据，响应式数据可以直接通过组件实例访问
+    return {
+      msg: '哈哈哈哈哈'
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <h2>{{ name }}</h2>
+  </div>
+</template>
---
+++ 03_vue/02_vue的基础/src/main.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: cavonLucky 425247833@qq.com
+ * @Date: 2023-05-06 20:02:17
+ * @LastEditors: cavonLucky 425247833@qq.com
+ * @LastEditTime: 2023-05-06 21:03:56
+ * @FilePath: /vue-course/03_vue/02_vue的基础/src/main.js
+ */
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+/**
+ * App.vue 是根组件
+ *  - createApp(App) 将根组件关联到应用上
+ *    - 会返回一个应用的实例
+ *  - app.mount("#app")将应用挂载到页面中
+ *    - 会返回一个根组件的实例，组件的实例通常可以命名为vm
+ *    - 组件实例是一个 Proxy对象（代理对象）
+ */
+
+// createApp(App).mount('#app');
+
+const app = createApp(App);
+console.log(app);
+
+const vm = app.mount('#app');
+console.log(vm);
+
+// 将 vm 设置为全局变量
+window.vm = vm;
