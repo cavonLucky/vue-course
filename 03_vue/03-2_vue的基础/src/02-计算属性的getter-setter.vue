51e482cc6a277bc5bb55f0c5b39bdf735060abae
chengxy666 <425247833@qq.com>
2023-05-22 20:07:08 +0800

    计算属性getter/setter“

--- 03_vue/03-2_vue的基础/src/02-计算属性的getter-setter.vue
+++ 03_vue/03-2_vue的基础/src/02-计算属性的getter-setter.vue
@@ -2,7 +2,7 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-05-19 11:32:48
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-05-22 16:42:48
+ * @LastEditTime: 2023-05-22 20:03:38
  * @FilePath: /vue-course/03_vue/04_vue的基础/src/App.vue
 -->
 
@@ -14,7 +14,12 @@
     <h2>评语：{{ info }}</h2>
     <h2>methods: {{ getInfo() }}</h2>
     <button @click="uodateAge">减龄</button>
+    <hr>
     <!-- <h3>{{ info }}</h3> -->
+    <!-- <h3>{{ lastName + firstName }}</h3> -->
+    <h3>{{ name }}</h3>
+    <hr>
+    <h2>{{ arr[2] }} -- {{ arr[1] }} -- {{ arr[0] }}</h2>
   </div>
 </template>
 
@@ -24,11 +29,14 @@ export default {
   data() {
     return {
       stu: {
-        name: 'ffffff',
+        name: 'learn-vue',
         age: 11,
         gender: '男'
       },
-      msg: '今天大风呼呼的吹～～'
+      msg: '今天大风呼呼的吹～～',
+      firstName: '嘿嘿嘿嘿哈',
+      lastName: '学习学习学习',
+      arr: ['猪八戒', '沙和尚', '孙悟空']
     }
   },
   methods: {
@@ -57,10 +65,30 @@ export default {
    * - 会对数据进行缓存
    */
   computed: {
+    // 在计算属性的getter中，尽量只做读取相关的逻辑，不要执行那些会产生(副)作用的代码
     info() {
       // return '哈哈哈';
       console.log('info 调用');
       return this.stu.age >= 18 ? '嫩是一个成年人了' : '嫩不是一个成年人';
+    },
+    // 计算属性的简写（只有getter时）
+    // name() {
+    //   return this.lastName + this.firstName;
+    // }
+
+    // 可以为计算属性设置setter，使得计算属性可写，但是不建议这么做
+    name:{
+      get() {
+        return this.lastName + this.firstName;
+      },
+      // set 在计算属性被修改时调用
+      set(value) {
+        this.lastName = value[0];
+        this.firstName = value.slice(1);
+        console.log(value[0]);
+        console.log(value.slice(1));
+        console.log(value);
+      }
     }
   }
 }
