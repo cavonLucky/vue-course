f87b66bbc2db34be23e74001439b8da566e92ec9
chengxy666 <425247833@qq.com>
2023-05-22 15:33:39 +0800

    methods

--- 03_vue/04_vue的基础/src/main.js
+++ 03_vue/04_vue的基础/src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
+/*
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-05-19 11:32:48
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-05-19 18:15:59
+ * @FilePath: /vue-course/03_vue/04_vue的基础/src/main.js
+ */
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+console.log(app);
+
+const vm = app.mount('#app');
+console.log(vm);
+
+window.vm = vm;
+window.app = app;
+
+
