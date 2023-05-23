e182b5a308b0fbf51334a4b1098f85f099562e4f
chengxy666 <425247833@qq.com>
2023-05-23 18:41:20 +0800

    模板的语法

--- 03_vue/04_模板/src/main.js
+++ 03_vue/04_模板/src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+console.log(app);
+
+// 设置之前想一下需不需要这个全局变量...
+// app.config.globalProperties.hello = '你好，我是全局的属性';
+// app.config.globalProperties.alert = alert;
+// app.config.globalProperties.alert = alert.bind(this);
+
+const vm = app.mount('#app');
+console.log(vm);
+
+window.vm = vm;
+window.app = app;
+
