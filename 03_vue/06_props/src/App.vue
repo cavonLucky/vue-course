07069e0511203a9c9235045f3b6e6bcf0dbbac86
chengxy666 <425247833@qq.com>
2023-05-29 15:42:08 +0800

    初始化项目

---
+++ 03_vue/06_props/src/App.vue
@@ -0,0 +1,11 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-05-29 15:39:12
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-05-29 15:41:35
+ * @FilePath: /vue-course/03_vue/06_props/src/App.vue
+-->
+
+<template>
+  <div>哈哈哈哈</div>
+</template>
---
+++ 03_vue/06_props/src/main.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-05-29 15:39:12
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-05-29 15:41:10
+ * @FilePath: /vue-course/03_vue/06_props/src/main.js
+ */
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
