9fe2e20aa5ccea8c1aa2fb098a9418f16f936e8b
chengxy666 <425247833@qq.com>
2023-05-29 16:29:21 +0800

    props传值

--- 03_vue/06_props/src/App.vue
+++ 03_vue/06_props/src/App.vue
@@ -2,10 +2,28 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-05-29 15:39:12
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-05-29 15:41:35
+ * @LastEditTime: 2023-05-29 16:27:20
  * @FilePath: /vue-course/03_vue/06_props/src/App.vue
 -->
 
+<script setup>
+  import MyBox from './components/MyBox.vue';
+  import { ref } from 'vue';
+
+  const count = ref(0);
+  const obj = ref({
+    count: 0
+  });
+
+</script>
+
 <template>
-  <div>哈哈哈哈</div>
+  <div>
+    <h1>我是父组件</h1>
+    <!-- <MyBox a="111" /> -->
+    <!-- <MyBox a="111" b="33333" /> -->
+    <!-- <MyBox :count="count" :obj="obj" maxLength="100" /> -->
+    <MyBox :count="count" :obj="obj" max-length="100" />
+    <button @click="obj.count++">点我一下</button>
+  </div>
 </template>
---
+++ 03_vue/06_props/src/components/MyBox.vue
@@ -0,0 +1,38 @@
+
+
+<script setup>
+/**
+ * 父组件可以通过 props 来像子组件传递数据
+ * 注意：父组件传递给子组件的props都是只读的，无法修改，
+ *      即使可以修改，我们也尽量不要在子组件中去修改父组件的数据
+ *      如果非的要改，可以通过自定义事件
+ * 
+ * 属性名
+ *    - 定义属性名时，属性名要遵循驼峰命名法
+ */
+
+// 定义props
+// const props = defineProps(['a']);
+// const props = defineProps(['a', 'b']);
+const props = defineProps(['count', 'obj', 'maxLength']);
+console.log(props);
+</script>
+
+<template>
+  <div>
+    <hr>
+    <!-- <h2>我是子组件 MyBox {{ props.count }}</h2> -->
+    <!-- <button @click="props.count++">MyBox 的按钮</button> -->
+
+    <h2>我是子组件 MyBox {{ props.obj.count }}</h2>
+    <button @click="props.obj.count++">MyBox 的按钮</button>
+    <h2>{{ props.maxLength }}</h2>
+    <hr>
+  </div>
+</template>
+
+<style scoped>
+div {
+  color: tomato;
+}
+</style>
