42d747d010f68ce03b18bac63fe9c699df12051e
chengxy666 <425247833@qq.com>
2023-06-02 13:49:28 +0800

    插槽的简介

--- 03_vue/06_props/src/App.vue
+++ 03_vue/06_props/src/App.vue
@@ -1,98 +1,32 @@
 <!--
  * @Author: chengxy666 425247833@qq.com
- * @Date: 2023-05-31 12:20:46
+ * @Date: 2023-06-02 13:27:25
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-05-31 13:06:54
+ * @LastEditTime: 2023-06-02 13:47:12
  * @FilePath: /vue-course/03_vue/06_props/src/App.vue
 -->
 
 <script setup>
-import { ref } from 'vue';
-
-const arr = ref(['孙悟空', '猪八戒', '沙和尚', '唐僧']);
-const arr2 = ref([
-  { id: 1, name: '孙悟空', age: 18 },
-  { id: 2, name: '猪八戒', age: 28 },
-  { id: 3, name: '沙和尚', age: 38 }
-]);
-
+import MyButton from './components/MyButton.vue';
+import A from './components/A.vue';
 </script>
 
 <template>
-  <button @click="arr.push('白骨精')">点我一下</button>
-  <!-- <button @click="arr2.push({ id: 4, name: '唐僧', age: 22 })">点我一下2</button> -->
-  <button @click="arr2.unshift({ id: 4, name: '唐僧', age: 22 })">点我一下2</button>
-  <ul>
-    <li v-for="name of arr">{{ name }}</li>
-  </ul>
-
-  <ul>
-    <li v-for="(name, index) in arr">{{ index }} - {{ name }}</li>
-  </ul>
-
-  <div v-for="obj in arr2">{{ obj.name }} -- {{ obj.age }}</div>
-
-  <hr>
-
-  <!-- <template v-for="obj in arr2">{{ obj.name }} -- {{ obj.age }}</template> -->
-  <!-- <hr> -->
-
-  <!-- <template v-for="{ id, age, name } in arr2">{{ name }} -- {{ age }}</template> -->
-
+  <h1>App组件</h1>
   <!--
-    我们在使用v-for遍历时，用的结构和新的结构是按照顺序进行对比的
+    希望在父组件中指定子组件中的内容
+      - 我们可以通过插槽（slot）来实现该需求
 
-    旧的结构：
-    <div>孙悟空</div>
-    <div>猪八戒</div>
-    <div>沙和尚</div>
+    <MyButton>插槽的入口</MyButton>
 
-    新的结构(push)：
-    <div>孙悟空</div>
-    <div>猪八戒</div>
-    <div>沙和尚</div>
-    <div>唐僧</div>
+    <button>
+      <slot></slot> 插槽的出口
+    </button>
 
-    旧：
-    <div>孙悟空</div>
-    <div>猪八戒</div>
-    <div>沙和尚</div>
-
-    新（unshift）：
-    <div>唐僧</div>
-    <div>孙悟空</div>
-    <div>猪八戒</div>
-    <div>沙和尚</div>
-
-    在使用v-for时，可以为元素指定一个唯一的key，有了key以后，元素在比较时就会按照相同的key去比较，而不是顺序(注意不要用index，一般用id)
-
-    旧：
-    <div>孙悟空</div>
-    <div>猪八戒</div>
-    <div>沙和尚</div>
-
-    新（unshift）：
-    <div key="4">唐僧</div>
-    <div key="1">孙悟空</div>
-    <div key="2">猪八戒</div>
-    <div key="3">沙和尚</div>
-
-   -->
-  <!-- <div v-for="{ id, age, name } in arr2">{{ name }} -- {{ age }}</div> -->
-  <!-- <ul>
-    <li v-for="{ id, age, name } in arr2">
-      {{ name }} -- {{ age }}
-      <input type="text" name="" id="">
-    </li>
-  </ul> -->
-  <ul>
-    <!-- <li v-for="({ id, age, name }, index) in arr2" :key="index">
-      {{ name }} -- {{ age }}
-      <input type="text" name="" id="">
-    </li> -->
-    <li v-for="{ id, age, name } in arr2" :key="id">
-      {{ name }} -- {{ age }}
-      <input type="text" name="" id="">
-    </li>
-  </ul>
+  -->
+  <!-- <MyButton /> -->
+  <!-- <MyButton>这是App自定义的文字</MyButton> -->
+  <!-- <MyButton>嘻嘻嘻</MyButton> -->
+  <!-- <MyButton><div>哈哈哈</div></MyButton> -->
+  <MyButton><A></A></MyButton>
 </template>
---
+++ 03_vue/06_props/src/components/MyButton.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-02 13:29:16
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-02 13:48:32
+ * @FilePath: /vue-course/03_vue/06_props/src/components/MyButton.vue
+-->
+
+<template>
+  <!-- <button>点我一下</button> -->
+  <button>
+    <!-- slot标签会被组件的标签体所替换 -->
+    <slot></slot>
+  </button>
+</template>
