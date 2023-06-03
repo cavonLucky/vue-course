b45f240ac4e1ccccb91f260f3503a8c4103ba8c9
chengxy666 <425247833@qq.com>
2023-06-03 16:52:49 +0800

    透传属性

--- 03_vue/06_props/src/App.vue
+++ 03_vue/06_props/src/App.vue
@@ -1,75 +1,32 @@
 <!--
  * @Author: chengxy666 425247833@qq.com
- * @Date: 2023-06-03 11:50:43
+ * @Date: 2023-06-03 16:07:30
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-03 13:01:19
+ * @LastEditTime: 2023-06-03 16:52:24
  * @FilePath: /vue-course/03_vue/06_props/src/App.vue
 -->
 
 <script setup>
-// function clickHandler(text, event) {
-//   event.stopPropagation();
-//   alert(text);
-// }
-
-function clickHandler(text) {
-  alert(text);
-}
-
+import C from './components/C.vue';
 /**
- * 事件修饰符：
- *    .stop       停止事件的传播？似乎stop只能停止冒泡
- *    .capture    在捕获阶段触发事件
- *    .prevent    取消默认行为
- *    .self       只有事件由自身出发时才会有效
- *    .once       绑定一个一次性的事件
- *    .passive    主要用于提升滚动事件的性能
+ * 透传属性
+ *    - 在组件上设置属性，会自动传递给组件的根元素
+ *    - 这样一来可以方便我们在父组件中为子组件来设置属性
+ *    - 透传会发生在没有被声明为 props和emit的属性上
+ *    - 自动的透传只适用单根组件
  */
 
+function showMsg() {
+  alert('hello');
+}
 </script>
 
 <template>
   <div>
     <h1>哈哈哈</h1>
-    <!-- <div class="box1" @click="clickHandler('box1', $event)">
-      <div class="box2" @click="clickHandler('box2', $event)">
-        <div class="box3" @click="clickHandler('box3', $event)"></div>
-      </div>
-    </div> -->
-    <!-- <div class="box1" @click="clickHandler('box1')">
-      <div class="box2" @click.stop="clickHandler('box2')">
-        <div class="box3" @click.stop="clickHandler('box3')"></div>
-      </div>
-    </div> -->
-    <!-- <div class="box1" @click.capture="clickHandler('box1')">
-      <div class="box2" @click="clickHandler('box2')">
-        <div class="box3" @click.stop="clickHandler('box3')"></div>
-      </div>
-    </div> -->
-    <div class="box1" @click.self="clickHandler('box1')">
-      <div class="box2" @click.once="clickHandler('box2')">
-        <div class="box3" @click="clickHandler('box3')"></div>
-      </div>
-    </div>
+    <h2 class="box1">我是h2</h2>
+    <C  class="box2" style="color: red;" @click="showMsg"/>
   </div>
 </template>
 
-<style scoped>
-.box1 {
-  width: 300px;
-  height: 300px;
-  background-color: aquamarine;
-}
-
-.box2 {
-  width: 200px;
-  height: 200px;
-  background-color: yellow;
-}
-
-.box3 {
-  width: 100px;
-  height: 100px;
-  background-color: green;
-}
-</style>
+<style scoped></style>
---
+++ 03_vue/06_props/src/components/C.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-03 16:08:52
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-03 16:52:02
+ * @FilePath: /vue-course/03_vue/06_props/src/components/C.vue
+-->
+
+<template>
+  <!-- <h2>我是C组件</h2> -->
+  <h2 class="box3" style="font-size: 60px;">我是组件C</h2>
+  <h3>我也是组件C</h3>
+  <!--
+    在模板中，可以通过$attrs来访问透传过来的属性，可以手动指定透传过来的属性要添加到哪些元素
+   -->
+  {{ $attrs }}
+  <h2 class="box3" :class="$attrs.class" style="font-size: 60px;">我是组件C</h2>
+  <!-- <h3 :class="$attrs.class" :style="$attrs.style">我也是组件C</h3> -->
+  <h3 :="$attrs">我也是组件C</h3>
+</template>
