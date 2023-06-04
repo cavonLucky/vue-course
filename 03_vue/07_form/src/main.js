5ff92b69024614261e7eb8e976d645d8c0cf0fbe
chengxy666 <425247833@qq.com>
2023-06-04 11:18:35 +0800

    单向、双向数据绑定

---
+++ 03_vue/07_form/src/main.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-03 17:17:55
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-03 17:19:34
+ * @FilePath: /vue-course/03_vue/07_form/src/main.js
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
