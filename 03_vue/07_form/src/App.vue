bd2833ad93f52d048247ba6dbd0f30a3d5f47ca6
chengxy666 <425247833@qq.com>
2023-06-04 15:22:09 +0800

    实现删除功能

--- 03_vue/07_form/src/App.vue
+++ 03_vue/07_form/src/App.vue
@@ -1,92 +1,28 @@
 <!--
  * @Author: chengxy666 425247833@qq.com
- * @Date: 2023-06-04 11:43:55
+ * @Date: 2023-06-04 13:56:09
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-04 13:53:56
+ * @LastEditTime: 2023-06-04 14:44:53
  * @FilePath: /vue-course/03_vue/07_form/src/App.vue
 -->
 <script setup>
-// let text = '';
-// let text = '默认值';
 
+import StudentList from './components/StudentList.vue';
 import { ref } from 'vue';
-// 双向绑定
-let text = ref('');
-// let bool = ref(false);
-let bool = ref('是');
-let hobbies = ref([]);
-let gender = ref('女');
-// let friend = ref('孙悟空');
-let friend = ref('');
 
-function sunmitHandler() {
-  // console.log(text);
-  console.log(text.value);
-  // 将text提交给服务器，在根据服务器返回的数据做后续的操作
-}
+// 发送请求来向服务器加载数据
+const STU_ARR = ref([
+  { id: 1, name: '孙悟空', age: 18, gender: '男', address: '花果山' },
+  { id: 2, name: '猪八戒', age: 28, gender: '男', address: '高老庄' },
+  { id: 3, name: '沙和尚', age: 38, gender: '男', address: '流沙河' },
+  { id: 4, name: '唐僧', age: 48, gender: '男', address: '女儿国' }
+]);
 
 </script>
 
 <template>
   <div>
-    <h1>哈哈哈哈哈</h1>
-    <!-- @submit.prevent 取消浏览器地址栏默认跳转 -->
-    <form action="#" method="get" @submit.prevent="sunmitHandler">
-      <!--
-        v-model 修饰符
-          .lazy 使用chenge来处理数据
-          .trim 去除前后空格
-          .number 将数据转换为数值
-       -->
-      <div>
-        <!-- 信息：<input type="text" v-model="text" /> -->
-        <!-- 信息：<input type="text" v-model.lazy="text" /> -->
-        <!-- 信息：<input type="text" v-model.lazy.trim="text" /> -->
-        信息：<input type="text" v-model.lazy.trim.number="text" />
-      </div>
-
-      <div>
-        <!-- 是否：<input type="checkbox" v-model="bool" /> -->
-        <!-- 是否：<input type="checkbox" v-model="bool" true-value="是" /> -->
-        是否：<input type="checkbox" v-model="bool" true-value="是" false-value="否" />
-      </div>
-
-      <div>
-        爱好：
-        <input type="checkbox" name="hobby" v-model="hobbies" value="篮球" />
-        <input type="checkbox" name="hobby" v-model="hobbies" value="足球" />
-        <input type="checkbox" name="hobby" v-model="hobbies" value="羽毛球" />
-        <input type="checkbox" name="hobby" v-model="hobbies" value="乒乓球" />
-      </div>
-
-      <div>
-        性别：
-        <input type="radio" name="gender" v-model="gender" value="男">男
-        <input type="radio" name="gender" v-model="gender" value="女">女
-      </div>
-
-      <div>
-        朋友：
-        <!-- <select v-model="friend" multiple>
-          <option>孙悟空</option>
-          <option>猪八戒</option>
-          <option>沙和尚</option>
-          <option>唐僧</option>
-        </select> -->
-
-        <select v-model="friend">
-          <option disabled value="">请选择你的好朋友</option>
-          <option>孙悟空</option>
-          <option>猪八戒</option>
-          <option>沙和尚</option>
-          <option>唐僧</option>
-        </select>
-      </div>
-
-      <div>
-        <button>提交</button>
-      </div>
-    </form>
+    <StudentList :students="STU_ARR" />
   </div>
 </template>
 
---
+++ 03_vue/07_form/src/components/StudentList.vue
@@ -0,0 +1,82 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-04 14:29:57
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-04 15:19:50
+ * @FilePath: /vue-course/03_vue/07_form/src/components/StudentList.vue
+-->
+
+<script setup>
+
+/*
+  通过defineProps定义的属性在attrs中就不存在了
+  使用自定义属性时，最好通过defineProps来声明一下
+*/
+let props = defineProps(['students']);
+
+// 定义一个删除学生的方法
+const delStuHandler = (index) => {
+  if (confirm('该操作不可恢复，请确认')) {
+    // 删除当前学生
+    props.students.splice(index, 1); // props.students 是app组件（父组件）传过来的，不建议这样使用
+  }
+}
+
+</script>
+
+<template>
+  <table>
+    <caption>学生列表</caption>
+    <thead>
+      <tr>
+        <th>学号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>性别</th>
+        <th>住址</th>
+        <th>操作</th>
+      </tr>
+    </thead>
+    <tbody>
+      <!-- 不推荐使用 -->
+      <!-- {{ $attrs.students }} -->
+      <!-- <tr v-for="stu in $attrs.students" :key="stu.id">
+        <td>{{ stu.id }}</td>
+        <td>{{ stu.name }}</td>
+        <td>{{ stu.age }}</td>
+        <td>{{ stu.gender }}</td>
+        <td>{{ stu.address }}</td>
+      </tr> -->
+
+      <!-- 这样使用更规范 -->
+      <!-- {{ props.students }} -->
+      <tr v-for="(stu, index) in props.students" :key="stu.id">
+        <td>{{ stu.id }}</td>
+        <td>{{ stu.name }}</td>
+        <td>{{ stu.age }}</td>
+        <td>{{ stu.gender }}</td>
+        <td>{{ stu.address }}</td>
+        <td><a href="javascript:;" @click.prevent="delStuHandler(index)">删除</a></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  width: 80%;
+  border-collapse: collapse;
+  text-align: center;
+}
+
+caption {
+  font-size: 30px;
+  font-weight: bold;
+}
+
+th,
+td {
+  border: 1px solid;
+  font-size: 24px;
+}
+</style>
