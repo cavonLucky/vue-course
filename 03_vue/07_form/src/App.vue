23efb70100495cc84cc114f7a8f489560e6eddc0
chengxy666 <425247833@qq.com>
2023-06-05 12:06:43 +0800

    完成学生添加

--- 03_vue/07_form/src/App.vue
+++ 03_vue/07_form/src/App.vue
@@ -2,12 +2,13 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-06-04 13:56:09
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-04 16:18:14
+ * @LastEditTime: 2023-06-05 12:06:10
  * @FilePath: /vue-course/03_vue/07_form/src/App.vue
 -->
 <script setup>
 
 import StudentList from './components/StudentList.vue';
+import StudentForm from './components/StudentForm.vue';
 import { ref } from 'vue';
 
 // 发送请求来向服务器加载数据
@@ -24,6 +25,23 @@ const delStudentByIndex = (index) => {
   console.log('删除学生', index);
 }
 
+// 创建一个ref来存储新的学生信息
+// const newStu = ref({
+//   name: '',
+//   age: 1,
+//   gender: '男',
+//   address: ''
+// });
+
+// 添加学生的方法
+const addNewStudent = (students) => {
+  // console.log(students);x
+  const lastId = STU_ARR.value.at(-1)?.id;
+  const newID = !isNaN(lastId) ? lastId + 1 : 1;
+  students.id = newID;
+  STU_ARR.value.push(students);
+}
+
 </script>
 
 <template>
@@ -32,6 +50,8 @@ const delStudentByIndex = (index) => {
 
     <!-- 可以将组件中的方法以自定义事件的形式发送给其他的组件 -->
     <StudentList :students="STU_ARR" @del-stu="delStudentByIndex" />
+    <!-- <StudentForm :new-stu="newStu" /> -->
+    <StudentForm @add-student="addNewStudent" />
   </div>
 </template>
 
---
+++ 03_vue/07_form/src/components/StudentForm.vue
@@ -0,0 +1,63 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-04 16:25:53
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-05 12:05:58
+ * @FilePath: /vue-course/03_vue/07_form/src/components/StudentForm.vue
+-->
+
+<script setup>
+// const props = defineProps(['newStu']);
+// console.log(props);
+
+import { ref } from 'vue';
+
+const emits = defineEmits(['addStudent']);
+
+const newStu = ref({
+  name: '',
+  age: 1,
+  gender: '男',
+  address: ''
+});
+
+const submitHandler = () => {
+  // console.log(newStu.value);
+  // 调用方法，将newStu添加到数据中
+  // emits('addStudent', Object.assign({}, newStu.value));
+  emits('addStudent', { ...newStu.value });
+
+  newStu.value.name = '';
+  newStu.value.age = 1;
+  newStu.value.gender = '男';
+  newStu.value.address = '';
+}
+
+
+</script>
+
+<template>
+  <form action="" method="get" @submit.prevent="submitHandler">
+    <div>
+      姓名：<input type="text" name="" v-model="newStu.name">
+    </div>
+
+    <div>
+      年龄：<input type="number" name="" min="1" v-model="newStu.age">
+    </div>
+
+    <div>
+      性别：
+      <input type="radio" name="gender" value="男" v-model="newStu.gender">男
+      <input type="radio" name="gender" value="女" v-model="newStu.gender">女
+    </div>
+
+    <div>
+      住址：<input type="text" name="" v-model="newStu.address">
+    </div>
+
+    <div>
+      <button>添加</button>
+    </div>
+  </form>
+</template>
