0aea8119561d2dd3bc0b701801ea83699321bcca
chengxy666 <425247833@qq.com>
2023-06-05 12:46:00 +0800

    拆分组件使用透传引出的问题

---
+++ 03_vue/07_form/src/components/StudentItem.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-05 12:18:46
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-05 12:44:10
+ * @FilePath: /vue-course/03_vue/07_form/src/components/StudentItem.vue
+-->
+
+<script setup>
+const props = defineProps(['students']);
+const emits = defineEmits(['delStu']);
+
+// 定义一个删除学生的方法
+const delStuHandler = (index) => {
+  if (confirm('该操作不可恢复，请确认')) {
+    emits('delStu', index);
+  }
+}
+
+</script>
+
+<template>
+  <tbody>
+    <tr v-for="(stu, index) in props.students" :key="stu.id">
+      <td>{{ stu.id }}</td>
+      <td>{{ stu.name }}</td>
+      <td>{{ stu.age }}</td>
+      <td>{{ stu.gender }}</td>
+      <td>{{ stu.address }}</td>
+      <!-- <td><a href="javascript:;" @click.prevent="delStuHandler(index)">删除</a></td> -->
+      <td><a href="javascript:;" @click.prevent="delStuHandler(index)">删除</a></td>
+    </tr>
+  </tbody>
+</template>
+
+<style scoped>
+td {
+  border: 1px solid;
+  font-size: 24px;
+}
+</style>
--- 03_vue/07_form/src/components/StudentList.vue
+++ 03_vue/07_form/src/components/StudentList.vue
@@ -2,36 +2,23 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-06-04 14:29:57
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-04 16:23:28
+ * @LastEditTime: 2023-06-05 12:44:01
  * @FilePath: /vue-course/03_vue/07_form/src/components/StudentList.vue
 -->
 
 <script setup>
 
-/*
-  通过defineProps定义的属性在attrs中就不存在了
-  使用自定义属性时，最好通过defineProps来声明一下
-*/
-let props = defineProps(['students', 'fn']);
-let emits = defineEmits(['delStu']);
+import StudentItem from './StudentItem.vue';
+
+// let props = defineProps(['students', 'fn']);
+// let emits = defineEmits(['delStu']);
 
 // // 定义一个删除学生的方法
 // const delStuHandler = (index) => {
 //   if (confirm('该操作不可恢复，请确认')) {
-//     // 删除当前学生
-//     props.students.splice(index, 1); // props.students 是app组件（父组件）传过来的，不建议这样使用
+//     emits('delStu', index);
 //   }
 // }
-
-const delStuHandler = (index) => {
-  if (confirm('该操作不可恢复，请确认')) {
-    // 自己的数据自己管，因此，可以在父组件定义一个函数来进行删除
-    // props.fn(index);
-    // props.fn(index);
-
-    emits('delStu', index);
-  }
-}
 
 </script>
 
@@ -48,33 +35,8 @@ const delStuHandler = (index) => {
         <th>操作</th>
       </tr>
     </thead>
-    <tbody>
-      <!-- 不推荐使用 -->
-      <!-- {{ $attrs.students }} -->
-      <!-- <tr v-for="stu in $attrs.students" :key="stu.id">
-        <td>{{ stu.id }}</td>
-        <td>{{ stu.name }}</td>
-        <td>{{ stu.age }}</td>
-        <td>{{ stu.gender }}</td>
-        <td>{{ stu.address }}</td>
-      </tr> -->
-
-      <!-- 这样使用更规范 -->
-      <!-- {{ props.students }} -->
-      <tr v-for="(stu, index) in props.students" :key="stu.id">
-        <td>{{ stu.id }}</td>
-        <td>{{ stu.name }}</td>
-        <td>{{ stu.age }}</td>
-        <td>{{ stu.gender }}</td>
-        <td>{{ stu.address }}</td>
-        <!-- <td><a href="javascript:;" @click.prevent="delStuHandler(index)">删除</a></td> -->
-        <!-- 在模板中可以通过 $emit() 来触发自定义事件 -->
-        <!-- <td><a href="javascript:;" @click.prevent="$emit('delStu', index)">删除</a></td> -->
-
-        <!-- <td><a href="javascript:;" @click.prevent="emits('delStu', index)">删除</a></td> -->
-        <td><a href="javascript:;" @click.prevent="delStuHandler(index)">删除</a></td>
-      </tr>
-    </tbody>
+    <!-- <StudentItem :students="props.students" @del-stu="emits('delStu', $event)" /> -->
+    <StudentItem :students="$attrs.students" @del-stu="$attrs.onDelStu" />
   </table>
 </template>
 
@@ -90,8 +52,7 @@ caption {
   font-weight: bold;
 }
 
-th,
-td {
+th {
   border: 1px solid;
   font-size: 24px;
 }
