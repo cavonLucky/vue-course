b1c555894a6f99ddf9ea686a24aab4d5d3189013
chengxy666 <425247833@qq.com>
2023-06-06 20:12:19 +0800

    配置pinia

--- 03_vue/09_meals/src/App.vue
+++ 03_vue/09_meals/src/App.vue
@@ -2,16 +2,21 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-06-06 17:50:59
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-06 17:56:30
+ * @LastEditTime: 2023-06-06 20:11:14
  * @FilePath: /vue-course/03_vue/09_meals/src/App.vue
 -->
 
 <script setup>
+import { useMealsStore } from './store/meals';
+
+
+const meals = useMealsStore();
+console.log(meals.data[0]);
+
 </script>
 
 <template>
   <h1>哈喽</h1>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- 03_vue/09_meals/src/main.js
+++ 03_vue/09_meals/src/main.js
@@ -1,6 +1,17 @@
-import './assets/main.css'
+/*
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-06 17:50:59
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-06 20:07:09
+ * @FilePath: /vue-course/03_vue/09_meals/src/main.js
+ */
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import './assets/main.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+
+const app = createApp(App);
+app.use(createPinia());
+app.mount('#app');
