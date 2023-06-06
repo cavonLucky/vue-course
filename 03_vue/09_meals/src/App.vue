5ba66975b7085161990b44ce8e2e71a3ce9d4359
chengxy666 <425247833@qq.com>
2023-06-06 18:55:44 +0800

    项目初始化

---
+++ 03_vue/09_meals/src/App.vue
@@ -0,0 +1,17 @@
+<!--
+ * @Author: chengxy666 425247833@qq.com
+ * @Date: 2023-06-06 17:50:59
+ * @LastEditors: chengxy666 425247833@qq.com
+ * @LastEditTime: 2023-06-06 17:56:30
+ * @FilePath: /vue-course/03_vue/09_meals/src/App.vue
+-->
+
+<script setup>
+</script>
+
+<template>
+  <h1>哈喽</h1>
+</template>
+
+<style scoped>
+</style>
