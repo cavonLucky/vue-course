b0194ea4786f78dd153eab8ea735d5135a79dca7
chengxy666 <425247833@qq.com>
2023-06-07 19:00:47 +0800

    初步完成filter组件

--- 03_vue/09_meals/src/App.vue
+++ 03_vue/09_meals/src/App.vue
@@ -2,13 +2,14 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-06-06 17:50:59
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-06 20:26:30
+ * @LastEditTime: 2023-06-07 18:18:29
  * @FilePath: /vue-course/03_vue/09_meals/src/App.vue
 -->
 
 <script setup>
 import { useMealsStore } from './store/meals';
 import Meals from './components/Meals/Meals.vue';
+import MealsFilter from './components/MealsFilter/MealsFilter.vue';
 
 
 const meals = useMealsStore();
@@ -17,7 +18,10 @@ console.log(meals.data[0]);
 </script>
 
 <template>
-  <Meals />
+  <div>
+    <MealsFilter />
+    <Meals />
+  </div>
 </template>
 
 <style scoped></style>
--- 03_vue/09_meals/src/components/MealsFilter/MealsFilter.vue
+++ 03_vue/09_meals/src/components/MealsFilter/MealsFilter.vue
@@ -2,11 +2,48 @@
  * @Author: chengxy666 425247833@qq.com
  * @Date: 2023-06-06 20:25:03
  * @LastEditors: chengxy666 425247833@qq.com
- * @LastEditTime: 2023-06-06 20:25:24
+ * @LastEditTime: 2023-06-07 18:57:12
  * @FilePath: /vue-course/03_vue/09_meals/src/components/MealsFilter/MealsFilter.vue
  * @Description: 对所有食物过滤的组件
 -->
 
 <template>
-  <h1>对所有食物过滤的组件</h1>
+  <div class="filter">
+    <div class="filter-inner">
+      <input type="text" placeholder="请输入关键字" class="filter-input">
+      <i class="ri-search-line"></i>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.filter {
+  padding: 20rem 40rem;
+  border-bottom: 1px solid #e6e6e6;
+}
+
+.filter-inner {
+  height: 80rem;
+  position: relative;
+  display: flex;
+  align-items: center;
+}
+
+.filter-input {
+  background-color: #f2f2f2;
+  border: none;
+  height: 100%;
+  width: 100%;
+  border-radius: 36rem;
+  outline: none;
+  padding: 0 20rem 0 80rem;
+}
+
+.filter-inner i {
+  color: rgb(185, 185, 185);
+  font-size: 36rem;
+  font-weight: bold;
+  position: absolute;
+  left: 20rem;
+}
+</style>
