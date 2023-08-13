315901c4246daa3ac93f50b5a3c49d6f728cd2d3
chengfun <425247833@qq.com>
2023-08-13 13:49:49 +0800

    完成订单列表

--- 03_vue/09_meals/src/components/Checkout/Checkout.vue
+++ 03_vue/09_meals/src/components/Checkout/Checkout.vue
@@ -2,14 +2,14 @@
  * @Author: chengfun 425247833@qq.com
  * @Date: 2023-07-31 21:15:21
  * @LastEditors: chengfun 425247833@qq.com
- * @LastEditTime: 2023-08-08 19:32:08
+ * @LastEditTime: 2023-08-13 13:49:27
  * @FilePath: /vue-course/03_vue/09_meals/src/components/Checkout/Checkout.vue
  * @Description: 结账的页面
 -->
 
 <script setup>
 import { useMealsStore } from "../../store/meals";
-
+import Counter from "../UI/Counter.vue";
 
 const porps = defineProps(['isShow']);
 const emits = defineEmits(['close']);
@@ -26,11 +26,29 @@ const meals = useMealsStore();
     <div class="details">
       <header>餐品详情</header>
       <div class="list">
-        <div v-for="item in meals.cartMelas" :key="item.id">
-          {{ item.title }}
+        <div class="item" v-for="item in meals.cartMelas" :key="item.id">
+          <div class="img-wrap">
+            <img :src="item.img" :alt="item.title">
+          </div>
+          <div class="info">
+            <h2 class="title">{{ item.title }}</h2>
+            <div class="count">
+              <Counter :meal="item" />
+              <span class="amount">{{ item.count * item.price }}</span>
+            </div>
+          </div>
         </div>
       </div>
       <footer>合计¥<span>{{ meals.amount }}</span></footer>
+    </div>
+    <div class="bar">
+      <div class="total-amount">
+        <p class="no-goods" v-show="meals.totalCount <= 0">未选购商品</p>
+        <p class="has-goods" v-show="meals.totalCount > 0" @click="showDetails = true">
+          {{ meals.amount }}
+        </p>
+      </div>
+      <button class="checkout-btn">去支付</button>
     </div>
   </div>
 </template>
@@ -98,5 +116,90 @@ const meals = useMealsStore();
 .details footer span {
   font-weight: bold;
   font-size: 30rem;
+}
+
+.list {
+  height: 800rem;
+  overflow: auto;
+}
+
+.item {
+  display: flex;
+}
+
+.img-wrap {
+  width: 160rem;
+}
+
+.info {
+  flex: auto;
+}
+
+.title {
+  margin: 26rem 0;
+  font-size: 26rem;
+}
+
+.count {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.amount {
+  font-size: 26rem;
+  font-weight: bold;
+}
+
+.amount::before {
+  content: '¥';
+  font-weight: normal;
+}
+
+.bar {
+  width: 710rem;
+  height: 112rem;
+  position: fixed;
+  background-color: #3a3a3a;
+  bottom: 30rem;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  border-radius: 60rem;
+}
+
+.total-amount {
+  margin-left: 30rem;
+  line-height: 112rem;
+}
+
+.no-goods,
+.has-goods {
+  display: inline-block;
+  color: #a9a9a9;
+  font-size: 40rem;
+  font-weight: bold;
+}
+
+.has-goods {
+  color: #fff;
+}
+
+.has-goods::before {
+  content: "合计¥";
+  font-size: 26rem;
+  color: #ccc;
+}
+
+.checkout-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 200rem;
+  height: 100%;
+  border: none;
+  border-radius: 60rem;
+  background-color: #efbe41;
+  font-size: 40rem;
 }
 </style>
